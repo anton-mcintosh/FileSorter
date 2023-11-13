1941642df2c1823f81bcb8ae3ab9e6c1f7ea5734
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2023-11-13 09:35:06 -0800

    ability added to sort screenshots folder by date

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 import os
 import shutil
+from datetime import datetime
 
-path = '/Users/anthonymcintosh/downloads'
+downloads = '/Users/anthonymcintosh/downloads'
+screenshots = '/Users/anthonymcintosh/desktop/screenshots'
 
-list_ = os.listdir(path)
-for file_ in list_:
-    name, ext = os.path.splitext(file_)
-    ext = ext[1:]
-    if ext == '':
-        continue
-    if os.path.exists(path+'/'+ext):
-        shutil.move(path+'/'+file_, path+'/'+ext+'/'+file_)
-    else:
-        os.makedirs(path+'/'+ext)
-        shutil.move(path+'/'+file_, path+'/'+ext+'/'+file_)
+def sortDownloads():
+    list_ = os.listdir(downloads)
+    for file_ in list_:
+        name, ext = os.path.splitext(file_)
+        ext = ext[1:]
+        if ext == '':
+            continue
+        if os.path.exists(downloads+'/'+ext):
+            shutil.move(downloads+'/'+file_, downloads+'/'+ext+'/'+file_)
+        else:
+            os.makedirs(downloads+'/'+ext)
+            shutil.move(downloads+'/'+file_, downloads+'/'+ext+'/'+file_)
+
+def sortScreenshots():
+    list_ = os.listdir(screenshots)
+    sorted_list = sorted(list_, key=lambda x: os.path.getctime(os.path.join(screenshots, x)))
+    for file_ in sorted_list:
+        name, ext = os.path.splitext(file_)
+        ext = ext[1:]
+        if ext == '':
+            continue
+
+        #Get the creation time of the file
+        creation_time = os.path.getctime(os.path.join(screenshots, file_))
+        creation_date = datetime.fromtimestamp(creation_time).date()
+
+        #Create the destination folder based on the creation date
+        dest_folder = os.path.join(screenshots, str(creation_date))
+
+
+        #Move the file to the destination folder
+        if os.path.exists(dest_folder):
+            shutil.move(os.path.join(screenshots, file_), os.path.join(dest_folder, file_))
+        else:
+            os.makedirs(dest_folder)
+            shutil.move(os.path.join(screenshots, file_), os.path.join(dest_folder, file_))
+
+sortDownloads()
+sortScreenshots()
